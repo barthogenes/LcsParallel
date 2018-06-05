0186a02834a3d0143fea6bf4f6c48461d9d1617e
Bart Hogenes <barthogenes@gmail.com>
2018-06-05 09:53:56 +0200

    Renamed lcs to core and added serial module

---
+++ serial/src/main/java/com/hva/parallel_computing/App.java
@@ -0,0 +1,13 @@
+package com.hva.parallel_computing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
